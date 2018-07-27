6253871584d6faea536ec1b64eb48c4edb1092eb
Jessica Mauerhan <jessicamauerhan@gmail.com>
2018-07-26 19:19:19 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -7,4 +7,20 @@ $result  = (object)[
     'message' => 'Hello! It is ' . $current
 ];
 
+$user = $_ENV['DB_USERNAME'];
+$pass = $_ENV['DB_PASSWORD'];
+$db   = $_ENV['DB_DATABASE'];
+$host = $_ENV['DB_HOST'];
+$dsn  = "pgsql:host={$host} user={$user} password={$pass} dbname={$db}";
+$pdo  = new PDO($dsn);
+
+$users = [];
+$sql   = "SELECT * FROM users";
+$row   = $pdo->query($sql);
+if ($row !== false) {
+    $users = $row->fetchAll(PDO::FETCH_COLUMN);
+}
+
+$result->users = $users;
+
 echo json_encode($result);
